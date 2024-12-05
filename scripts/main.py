aa973bcb32ca4af2628abfafde064afd89585f7f
Francesco Casadei <93782245+Cesco16@users.noreply.github.com>
2024-12-05 15:07:55 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -11,29 +11,35 @@ from bokeh.models import BoxZoomTool, ColumnDataSource, HoverTool
 from bokeh.models import PanTool, Range1d, ResetTool, WheelZoomTool
 from dominate.tags import h3, p, span, table, tbody, td, th, thead, tr
 
-from utils import
+from utils import bed_ru_merge, extract_sequences, create_plot_input_files
 
-#global variable
+## Global variables
 sample = 'vac06122023'#'native9411'#'native13204'#'native10498'
-repeat_motif = 'CAG'#'TAAAA'#'CAG'
-interrupt_motif = 'CAA'#'TGGAA'#'GAG'# 'GAG'
+repeat_motif = 'CAG' #'TAAAA'#'CAG'
+interrupt_motif = 'CAA' #'TGGAA'#'GAG'# 'GAG'
 rep_length = len(repeat_motif)
 int_length = len(interrupt_motif)
 
-pd_straglr = pd.read_csv('/home/PERSONALE/francesco.casadei20/GridIon/'+ sample +'/'+ sample +'_straglr.tsv', sep="\t", header=1)
-pd_stranger = pd.read_csv('/home/PERSONALE/francesco.casadei20/GridIon/'+ sample +'/'+ sample + '_rep_plot.tsv', sep="\t", header=0)
-#repeat_bed = pd.read_csv('/home/PERSONALE/francesco.casadei20/GridIon/STR.bed', sep="\t", header=1)
+path = "/home/PERSONALE/francesco.casadei20/GridIon/" #path where bam and vcf files are saved
+
+## Import straglr tsv file
+pd_straglr = pd.read_csv(path+ sample +'/'+ sample +'_straglr.tsv', sep="\t", header=1)
+## Import stranger annotated file
+pd_stranger = pd.read_csv(path+ sample +'/'+ sample + '_rep_plot.tsv', sep="\t", header=0)
+## Merge straglr and stranger files
 merged = pd.merge(pd_straglr, pd_stranger, left_on='start', right_on='POS')
-# Merging BED RU info
+
+## Merging BED RU info
 merged = bed_ru_merge(merged, '/home/PERSONALE/francesco.casadei20/GridIon/wf_str_repeats.bed')
-
+## Extract sequences information as a json file
 str_seq_json = extract_sequences('/home/PERSONALE/francesco.casadei20/GridIon/'+ sample +'/'+ sample +'_str_reads.bam', merged)
-
+## Extract relevant information for plot
 str_identifier = create_plot_input_files(str_seq_json)
 
-## creo una lista con tutti i frammenti della STR
+## Create a list with all the STR fragments
+## Import the created plot .csv file
+plot_file = pd.read_csv(path+sample+f"/{str_identifier}_str-content.csv")
 
-plot_file = pd.read_csv("/home/PERSONALE/francesco.casadei20/GridIon/"+sample+f"/{str_identifier}_str-content.csv")
 
 read_ids = plot_file['read_id'].unique()
 STR = []
