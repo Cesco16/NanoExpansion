a6fbfb927e86d2bf052118a9a48e669f6212a8a4
Francesco Casadei <93782245+Cesco16@users.noreply.github.com>
2025-08-21 15:29:58 +0200

    Update NanoExpansion.py

--- scripts/NanoExpansion.py
+++ scripts/NanoExpansion.py
@@ -35,13 +35,13 @@ int_length = len(interrupt_motif)
 ## Import straglr tsv file
 pd_straglr = pd.read_csv(path  +  '/' + sample + '/nanoexpansion/'+ sample + '-straglr.tsv', sep="\t", header=1)
 ## Import stranger annotated file
-pd_stranger = pd.read_csv(path + '/' + sample + '/nanoexpansion/native'  + sample + '_rep_plot.tsv', sep="\t", header=0)
+pd_stranger = pd.read_csv(path + '/' + sample + '/nanoexpansion/native'  + sample + '_plt.tsv', sep="\t", header=0)
 ## Merge straglr and stranger files
 merged = pd.merge(pd_straglr, pd_stranger, left_on='start', right_on='POS')
 ## Merging BED RU info
-merged = bed_ru_merge(merged, path  + '/' + 'wf_str_repeats.bed')
+merged = bed_ru_merge(merged, path  + '/' + 'str_repeats.bed')
 ## Extract sequences information as a json file
-str_seq_json = extract_sequences(path + '/' +  sample + '/nanoexpansion/native' + sample + '_str_reads.bam', merged, repeat_motif)
+str_seq_json = extract_sequences(path + '/' +  sample + '/nanoexpansion/native' + sample + '_reads.bam', merged, repeat_motif)
 ## Extract relevant information for plot
 str_identifier = create_plot_input_files(str_seq_json, sample, path)
 ## Create a list with all the STR fragments
